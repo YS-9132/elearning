98d0800f536f917fa7f81111a6370499461ac0a8
YS-9132 <nakano@mdsy.co.jp>
2026-02-26 11:33:56 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 SPREADSHEET_ID = '1Cl0TlNamAjIC4JfTpDOWc5IRpUJx3UqYhyiGXIZh5Mc'
 SENDER_EMAIL = 'nakano@mdsy.jp'
-
+GAS_URL = "https://script.google.com/macros/s/AKfycbxWSbmT-dsCZbdq7g8ptEf_3x82eMcPWOZbjkWB4OdihQmO_1VosMLOxoNIMBPxKaDo/exec"
